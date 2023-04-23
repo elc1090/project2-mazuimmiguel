2f74531ecc14e93363fb2918c19c9d228893b5a4
Miguel M Silva <mmsilva@inf.ufsm.br>
2023-04-22 23:00:25 -0300

    Base

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 var deckId;
-const deckForm = document.getElementById('deckForm');
 
-// Listen for submissions on GitHub username input form
-deckForm.addEventListener('submit', (e) => {
-
-    // Prevent default form submission action
-    e.preventDefault();
-
-    requestIdDeck()
+function newDeck(){
+    fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
         .then(response => response.json()) // parse response into json
         .then(data => {   
             deckId = data.deck_id;
             alert(deckId);            
         })
-})
+}
 
-function requestIdDeck() {
-    return Promise.resolve(fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`));
+const handForm = document.getElementById('handForm');
+
+function newHand(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(response => response.json()) // parse response into json
+        .then(data => {   
+        alert(data.cards[0].value +" "+ data.cards[0].suit);
+        alert(data.cards[1].value);           
+    })
 }
