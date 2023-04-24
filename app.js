1ea105f9c24a09d310e7b13544d2bd150d5ba29d
Miguel M Silva <mmsilva@inf.ufsm.br>
2023-04-24 20:20:31 -0300

    Jogo Base

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
 var deckId;
 var playerHand = [];
 var dealerHand = [];
+var dealerSum;
+var playerSum;
 
 window.onload = function() {
     newDeck();
 };
 
-function newDeck(){
-    fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
+async function newDeck(){
+    await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
         .then(response => response.json()) 
         .then(data => {   
-            deckId = data.deck_id;
-
-            let pdeck = document.getElementById('pDeckId');
-
-            pdeck.innerHTML = `Deckid = ${deckId}`;                        
+            deckId = data.deck_id;                      
         })
+
+    newGame();
 }
 
-function newHand(){
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+function newGame (){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
         .then(response => response.json()) 
         .then(data => {
 
-            let reset = document.getElementById('playerHand');
-            reset.innerHTML = "";
+            let resetPlayer = document.getElementById('playerHand');
+            resetPlayer.innerHTML = "";
+
+            let resetdealer = document.getElementById('dealerHand');
+            resetdealer.innerHTML = "";
 
             playerHand = [];
+
+            dealerHand = [];
 
             let hitButton = document.querySelector('#hitButton');
             hitButton.disabled = false;
 
-            for (let i in data.cards) {
+            let stopButton = document.querySelector('#stopButton');
+            stopButton.disabled = false;
+
+            for (let i = 0; i < 2; i++) {
                 let ul = document.getElementById('playerHand');
 
                 let li = document.createElement('li');
@@ -45,17 +53,9 @@ function newHand(){
 
                 playerHand.push(data.cards[i].value);
             }
-            
-            countCards();
-    })
-}
 
-function addCard(){
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-        .then(response => response.json()) 
-        .then(data => {
-            for (let i in data.cards) {
-                let ul = document.getElementById('playerHand');
+            for (let i = 2; i < 4; i++) {
+                let ul = document.getElementById('dealerHand');
 
                 let li = document.createElement('li');
 
@@ -64,58 +64,144 @@ function addCard(){
                 li.innerHTML = (`
                     <img src="${data.cards[i].images.png }" alt="card">
                 `);
-                ul.appendChild(li);
-                
-                playerHand.push(data.cards[i].value);
-            }
-            countCards();
-    })
+                ul.appendChild(li);  
 
+                dealerHand.push(data.cards[i].value);
+            }
+
+            countCards('playerHand');
+            countCards('dealerHand');
+        }) 
+}           
+            
+async function addCard(handType){
+    await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+        .then(response => response.json()) 
+        .then(data => {
+            if(handType === 'playerHand'){
+                let ul = document.getElementById('playerHand');
+                let li = document.createElement('li');
     
+                li.classList.add('list-group-item')
+    
+                li.innerHTML = (`
+                    <img src="${data.cards[0].images.png }" alt="card">
+                `);
+                ul.appendChild(li);
+                    
+                playerHand.push(data.cards[0].value);
+                countCards('playerHand');
+            }else{
+                let ul = document.getElementById('dealerHand');
+                let li = document.createElement('li');
+    
+                li.classList.add('list-group-item')
+    
+                li.innerHTML = (`
+                    <img src="${data.cards[0].images.png }" alt="card">
+                `);
+                ul.appendChild(li);
+                    
+                dealerHand.push(data.cards[0].value);
+                countCards('dealerHand');
+            }  
+    })
 }
 
-function countCards(){    
-    let pCounter = document.getElementById('playerCounter');
+function countCards(handType){    
     let sum = 0;
 
-    for(let i in playerHand){
-        switch(playerHand[i]){
-            case `ACE`:
-                sum +=1;
-                break;
-            case `QUEEN`:
-                sum +=10;
-                break;
-            case `KING`:
-                sum +=10;
-                break;
-            case `JACK`:
-                sum +=10;
-                break;
-            default:
-                sum += +playerHand[i];
-        }
-    }
-    pCounter.innerHTML = `Counter = ${sum}`;
+    if(handType === 'playerHand'){
+        let pCounter = document.getElementById('playerCounter');
+        for(let i in playerHand){
 
-    if(sum>21){
-        let hitButton = document.querySelector('#hitButton');
-        hitButton.disabled = true;
-    }
+            switch(playerHand[i]){
+                case `ACE`:
+                    if(playerHand.length === 2 && (playerHand[0]) != playerHand[1]){//Soft
+                        sum +=11; 
+                    }else{
+                        sum +=1;
+                    }
+                    break;
+                case `QUEEN`:
+                    sum +=10;
+                    break;
+                case `KING`:
+                    sum +=10;
+                    break;
+                case `JACK`:
+                    sum +=10;
+                    break;
+                default:
+                    sum += +playerHand[i];
+            }
+        }
+        pCounter.innerHTML = `Card Counter = ${sum}`;
+        playerSum = sum;
+    
+        if(sum>21){
+            let hitButton = document.querySelector('#hitButton');
+            hitButton.disabled = true;
+
+            let stopButton = document.querySelector('#stopButton');
+            stopButton.disabled = true;
+
+            resultMatch('bust');
+        }
+
+    }else{
+        let pCounter = document.getElementById('dealerCounter');
+        for(let i in dealerHand){
+            switch(dealerHand[i]){
+                case `ACE`:
+                    if(playerHand.length === 2){
+                        sum +=11; 
+                    }else{
+                        sum +=1;
+                    }
+                    break;
+                case `QUEEN`:
+                    sum +=10;
+                    break;
+                case `KING`:
+                    sum +=10;
+                    break;
+                case `JACK`:
+                    sum +=10;
+                    break;
+                default:
+                    sum += +dealerHand[i];
+            }
+        }
+        pCounter.innerHTML = `Card Counter = ${sum}`;
+        dealerSum = sum;
+    } 
 }
 
 function stop(){
     let hitButton = document.querySelector('#hitButton');
     hitButton.disabled = true;
 
+    let stopButton = document.querySelector('#stopButton');
+    stopButton.disabled = true;
     dealerAI();
 }
 
-function dealerAI(){
-    
+async function dealerAI(){
+    while(dealerSum <17){
+        await Promise.resolve(addCard('dealerHand')); 
+    }
+    resultMatch();
 }
 
-//data.cards[0].value 
-//data.cards[0].suit
-//data.cards[0].images.png 
-//https://deckofcardsapi.com/api/deck/new/draw/?count=2
+function resultMatch(result){
+    if(dealerSum > 21){
+        alert("Tu ganhou!");
+    }else if(result === "bust"){
+        alert("Passou de 21!");
+    }else if(dealerSum > playerSum){
+        alert("Menos que dealer!");
+    }else{
+        alert("Tu ganhou!");
+    }
+}
